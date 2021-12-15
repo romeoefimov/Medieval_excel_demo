6745d296bb2aedf87e1c9ac2576b1c6d24d3f38a
romeoefimov <refimov@edventure.ru>
2021-12-15 14:04:05 +0300

    15.12

---
+++ story_content/user.js
@@ -0,0 +1,323 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vwhrfgdwb8":
+        Script1();
+        break;
+      case "6T2T5CVx11F":
+        Script2();
+        break;
+      case "5fdnyI4dCmp":
+        Script3();
+        break;
+      case "6676JFM8rQJ":
+        Script4();
+        break;
+      case "6aSiTbxdkbj":
+        Script5();
+        break;
+      case "5Y8zQj86LWy":
+        Script6();
+        break;
+      case "5u8wmVr6xSJ":
+        Script7();
+        break;
+      case "6fe656khlGW":
+        Script8();
+        break;
+      case "5XT3P3tA3tb":
+        Script9();
+        break;
+      case "5v7H6fYBVN3":
+        Script10();
+        break;
+      case "5pSsaEk6Awp":
+        Script11();
+        break;
+      case "6DB0tuTXpcS":
+        Script12();
+        break;
+      case "6HbA2Kgin9b":
+        Script13();
+        break;
+      case "6FBkbPoqNvp":
+        Script14();
+        break;
+      case "5n8g0Dvmqoh":
+        Script15();
+        break;
+      case "5jx8NVc9V1k":
+        Script16();
+        break;
+      case "6piNrmXQy5Z":
+        Script17();
+        break;
+      case "6gfKP2oleK9":
+        Script18();
+        break;
+      case "6DJvpfr3QVv":
+        Script19();
+        break;
+      case "6EmnGZnhoon":
+        Script20();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+
+function add_line() {
+	var line = document.createElement("audio");
+	var head=document.getElementsByTagName('body')[0];
+	line.type = "audio/mp3";
+	line.src="";
+	line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = false;
+	head.appendChild(line);
+}
+
+//We only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+	var audio = document.getElementById('bgSong');
+	audio.volume = 0.1;
+	player.SetVar("javascriptsLoaded",true)
+}
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
+function Script19()
+{
+  var player = GetPlayer();
+
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+
+}
+
